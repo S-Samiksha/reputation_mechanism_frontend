9f9613a04da4917d525935cf90fd6adff3184bb2
S-Samiksha <samikshasankar01@gmail.com>
2024-03-03 12:47:11 +0800

    frontend contract abi and address retrieval

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 ### Things to do:
 1. Stopped at 17:08 
-2. make the connect button
 3. Add buyer
 4. Add seller
 5. Upload product
 6. purchase product 
 7. Leave a review 
+8. Styling
+
+
+### Things that are done
+1. Connect button
+2. Retrieving contractABI and contract Address
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,22 @@ import Head from "next/head"
 import { Inter } from "next/font/google"
 import Header from "../../Components/Header"
 import styles from "../styles/Home.module.css"
-import { userMoralis } from "react-moralis"
+import { useMoralis } from "react-moralis"
+import MarketEntrance from "../../Components/MarketEntrance"
 
 const inter = Inter({ subsets: ["latin"] })
+const supportedChains = ["31337", "11155111"]
 
 export default function Home() {
+    const { isWeb3Enabled, chainId } = useMoralis()
+
     return (
         <div className={styles.container}>
             <Head>
                 <title>E-Commerce Platform</title>
             </Head>
-            <Header></Header>
+            <Header />
+            <MarketEntrance />
         </div>
     )
 }
