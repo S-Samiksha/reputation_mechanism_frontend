1f58734384fa705afcbf74f8ef5ff82008e225b0
S-Samiksha <samikshasankar01@gmail.com>
2024-03-15 18:40:43 +0800

    front end styling fixed

--- Components/MarketEntrance.jsx
+++ Components/MarketEntrance.jsx
@@ -1,13 +1,33 @@
 import { useWeb3Contract } from "react-moralis"
+import { abi, contractAddress } from "../constants"
+import { useMoralis } from "react-moralis"
 
 export default function MarketEntrace() {
-    // const{runContractFunction: enterMarket} = useWeb3Contract{
-    //     abi: //,
-    //     contractAddress: //,
-    //     functionName: //,
-    //     params: {},
-    //     msgValue: //
-    // }
+    const { chainId: chainIdHex } = useMoralis()
+    const chainId = parseInt(chainIdHex)
+    const storeAddress = chainId in contractAddress ? contractAddress[chainId][0] : null
 
-    return <div>Hi!</div>
+    const { runContractFunction: createSeller } = useWeb3Contract({
+        abi: abi,
+        contractAddress: storeAddress,
+        functionName: "createSeller",
+        params: {},
+    })
+
+    return (
+        <div className="p-5">
+            {storeAddress ? (
+                <div>
+                    <h1 className="py-4 px-4 font-bold text-3xl">
+                        Contract Address: {storeAddress}
+                    </h1>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto">
+                        Create Seller
+                    </button>
+                </div>
+            ) : (
+                <div>No Store Address Detected </div>
+            )}
+        </div>
+    )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,11 @@
 //imports work with front end and require does not
 
-import Image from "next/image"
 import Head from "next/head"
-import { Inter } from "next/font/google"
 import Header from "../../Components/Header"
 import styles from "../styles/Home.module.css"
 import { useMoralis } from "react-moralis"
 import MarketEntrance from "../../Components/MarketEntrance"
 
-const inter = Inter({ subsets: ["latin"] })
 const supportedChains = ["31337", "11155111"]
 
 export default function Home() {
